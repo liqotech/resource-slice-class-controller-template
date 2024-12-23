54ff611453bacafddef6c3e8294d52707d7020da
Francesco Torta <62566275+fra98@users.noreply.github.com>
2024-12-23 17:31:49 +0100

    fix: delegate resources Condition update to the handler

--- pkg/resourceslice/handler/interface.go
+++ pkg/resourceslice/handler/interface.go
@@ -10,6 +10,14 @@ import (
 
 // Handler defines the interface for handling ResourceSlice operations.
 type Handler interface {
-	// Handle processes a ResourceSlice and returns a reconciliation result
+	// Handle processes a ResourceSlice and returns a reconciliation result.
+	// The handler should also update the ResourceSlice status. In particular, it should:
+	// - set the list of resources that have been allocated in `status.resources`
+	// - set a Condition of type "Resources"  in `status.conditions` to indicate
+	//   if the resources have been accepted or denied.
+	// An error should be returned if the handler fails to process the ResourceSlice
+	// and the reconciliation should be retried.
+	// Note: it should not return an error if the resourceslice has been correctly processed,
+	// but the resources have been denied.
 	Handle(ctx context.Context, resourceSlice *authv1beta1.ResourceSlice) (ctrl.Result, error)
 }
